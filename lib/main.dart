b73ae7392a0f0b58bbc039c851700845b2cf3830
Harsh <patelharsh749005@gmail.com>
2025-06-10 17:34:06 +0530

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/second_Page.dart';
+import 'package:todoapp/first_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
+  const MyApp({super.key});
 
-  List names = ["Mitch", "Sharon", "Vince"];
-  void userTapped(){
-    print("User Tapped!");
-  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text(
-            "My App Bar",
-            style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white),
-          ),
-          centerTitle: true,
-
-          backgroundColor: Colors.white,
-          elevation: 0,
-          leading: Icon(Icons.menu),
-          actions: [IconButton(onPressed: () {}, icon: Icon(Icons.logout))],
-          iconTheme: IconThemeData(color: Colors.white),
-        ),
-        body: Center(
-          child: GestureDetector(
-            onTap: userTapped,
-            child: Container(
-              width: 300,
-              height: 300,
-              color: Colors.pink,
-              child: Center(child: Text("Tap me!")),
-            ),
-          ),
-        ),
-      ),
+      home: FirstPage(),
+      routes: {
+        '/firstpage':(context) => FirstPage(),
+        '/secondpage':(context) => SecondPage(),
+      },
     );
   }
 }
